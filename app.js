eb568d5561885edd209a40d9596a6a65a650dbb2
Reno <erickjtorres03@gmail.com>
2026-06-24 11:47:06 -0400

    finished

--- app.js
+++ app.js
@@ -15,19 +15,79 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Books API is running"));
 
-app.get("/api/books", (req, res) => res.json(books));
+app.get("/api/books", (req, res) => {
+    console.log("i am getting all the books")
+    res.json(books)});  //.json( {an object can be put here like books was} )
 
-app.get("/api/books/:id", (req, res) => {
+app.post("/api/books", (req, res) => {
+    console.log('REQ BODY>>>>>>' , req.body)
+    // can do const {title, author, genre} = req.body
+    const newBook = {
+        id : nextId,
+        title: '',
+        author: '',
+        genre: '',
+        available : false
+    }
+    nextId++
+
+    books.push(newBook)
+
+    res.status(201).json({
+        done: "done",
+        newBook,
+        books
+    })
+})
+
+app.patch('/api/books/:bookId' , (req,res)=>{
+    const id = Number(req.params.bookId)
+    const { title } = req.body
     
-  const webId = Number(req.params.id) 
+    const mybook = null
+    books = books.map((book) => {
+        if (book.id === id){
+            return {...book, title: name} //dont forget to us {} since its an object
+        }
+        return book
+    })
+
+    if (!mybook) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+})
+
+app.delete('/api/books/:bookId', (req, res) => {
+    const id = Number(req.params.bookId)
+
+    books = books.filter((book)=>{
+        return book.id !== id
+    })
+
+    res.json({
+        delete: "well done",
+        books
+    })
+})
+
+app.get("/api/books/:id", (req, res) => {  //the :id can be named anything this is the dynamic segment
+    
+  const webId = Number(req.params.id) // req.params."dynamic segment" in this case its id
 
   const book = books.find((book) => book.id === webId)
-
+ // const mybook = null   another way to do this ^
+ // books.forEach(book => {
+ // if (book.id === webId) {
+ //     mybook = book; }})
   if (!book) {
     return res.status(404).json({ message: "Book not found" });
   }
+
+  // if (mybook == null) { return res.status(404).json({ message: "Book not found"}) } can be written this way too
     
   res.json(book);
     })
+
+
 
 app.listen(8080, () => console.log("Server running on port 8000"));
